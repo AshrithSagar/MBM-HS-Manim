117b31c851dae3db404ace4b55e0d164d85e7001
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-09-27 19:50:34 +0530

    c0.0.3

--- scene_1.py
+++ scene_1.py
@@ -1,12 +1,34 @@
 from manim import *
 
 
-def sequenceBoxes(string):
-    dimension = 2
+def mutationBoxes(boxGroup, seqGroup, mutations):
+    mainGroups, textGroups = [], []
+    for mutation in mutations:
+        position = mutation[0]
+        string = mutation[1]
 
+        mainGroup = VGroup()
+        textGroup = VGroup()
+        for index, character in enumerate(string[::-1]):
+            box = Square(
+                fill_color=GREEN,
+                fill_opacity=0.5,
+                stroke_color=GREEN
+            ).scale(0.5).shift((index-3) * UP)
+            text = Text(character, font_size=24).move_to(box.get_center())
+            mainGroup.add(box, text)
+            textGroup.add(text)
+        mainGroup.move_to(position*LEFT)
+        mainGroups.append(mainGroup)
+        textGroups.append(textGroup)
+
+    return mainGroups, textGroups
+
+
+def sequenceBoxes(sequence):
     mainGroup = VGroup()
     textGroup = VGroup()
-    for index, character in enumerate(string[::-1]):
+    for index, character in enumerate(sequence[::-1]):
         box = Square(
             fill_color=BLUE,
             fill_opacity=0.5,
@@ -19,22 +41,29 @@ def sequenceBoxes(string):
     return mainGroup, textGroup
 
 
-class CartesianProduct_2(Scene):
+class CartesianProduct(Scene):
     def construct(self):
         sequence = "SKNKCNE"
 
         # [[position, AA_groups], ...]
-        mutations = [[2, "NBCD"]]
+        mutations = [[2, "NDAQ"], [4, "CALEG"]]
 
-        box_group, seq_group = sequenceBoxes(string=sequence)
-        self.add(box_group)
+        seq_box_group, seq_text_group = sequenceBoxes(sequence)
+        mut_box_groups, mut_text_groups = mutationBoxes(
+            seq_box_group, seq_text_group, mutations)
+        self.add(seq_box_group)
 
-        # seq_group.to_corner(UP)
-        self.add(seq_group)
+        # seq_text_group.to_corner(UP)
+        self.add(seq_text_group)
 
-        # self.play(seq_group.animate.shift(3*DOWN))
+        for mut_box_group in mut_box_groups:
+            self.add(mut_box_group)
+        for mut_text_group in mut_text_groups:
+            self.add(mut_text_group)
+
+        # self.play(seq_text_group.animate.shift(3*DOWN))
 
         sequenceList = Tex("SKNKCNE")
         sequenceList.to_corner(UP + RIGHT)
-        self.play(Transform(seq_group, sequenceList))
+        self.play(Transform(seq_text_group, sequenceList))
         self.wait()
