d69d0566a8d05f82e68bf3293eece5910f8e04b6
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-10-01 19:38:47 +0530

    c0.1.2 Pylinted

--- scene_1.py
+++ scene_1.py
@@ -1,22 +1,28 @@
-from manim import *
+"""
+Cartesian Product animation of Peptide mutater using Manim
+"""
 import itertools
+from manim import *
 
 
-def mutationBoxes(sequence, boxGroup, seqGroup, mutations):
-    mutMainGroups, mutBoxGroups, mutTextGroups = [], [], []
+def mutation_boxes(sequence, box_group, seq_group, mutations):
+    """
+    For the vertical boxes containing the mutations
+    """
+    mut_main_groups, mut_box_groups, mut_text_groups = [], [], []
     sequence_length = len(sequence)
 
     for mutation in mutations:
         position = mutation[0]
-        string = mutation[1]
-        CorrespondingBox = boxGroup[sequence_length-position-1]
-        main_box_y = CorrespondingBox.get_y()
+        mutation_string = mutation[1]
+        corresponding_box = box_group[sequence_length-position-1]
+        main_box_y = corresponding_box.get_y()
 
-        mutBoxGroup = VGroup()
-        mutTextGroup = VGroup()
-        mutMainGroup = VGroup()
+        mut_box_group = VGroup()
+        mut_text_group = VGroup()
+        mut_main_group = VGroup()
 
-        for index, character in enumerate(string):
+        for index, character in enumerate(mutation_string):
             box = Square(
                 fill_color=GREEN,
                 fill_opacity=0.5,
@@ -24,22 +30,25 @@ def mutationBoxes(sequence, boxGroup, seqGroup, mutations):
             ).scale(0.5).shift((main_box_y-index) * UP)
             text = Text(character, font_size=48).move_to(box.get_center())
 
-            mutBoxGroup.add(box)
-            mutTextGroup.add(text)
-            mutMainGroup.add(box, text)
+            mut_box_group.add(box)
+            mut_text_group.add(text)
+            mut_main_group.add(box, text)
 
-        mutBoxGroup.match_x(CorrespondingBox)
-        mutTextGroup.match_x(CorrespondingBox)
-        mutMainGroups.append(mutMainGroup)
-        mutBoxGroups.append(mutBoxGroup)
-        mutTextGroups.append(mutTextGroup)
+        mut_box_group.match_x(corresponding_box)
+        mut_text_group.match_x(corresponding_box)
+        mut_main_groups.append(mut_main_group)
+        mut_box_groups.append(mut_box_group)
+        mut_text_groups.append(mut_text_group)
 
-    return mutMainGroups, mutBoxGroups, mutTextGroups
+    return mut_main_groups, mut_box_groups, mut_text_groups
 
 
-def sequenceBoxes(sequence):
-    boxGroup = VGroup()
-    textGroup = VGroup()
+def sequence_boxes(sequence):
+    """
+    Main horizontal sequence boxes
+    """
+    box_group = VGroup()
+    text_group = VGroup()
     for index, character in enumerate(sequence[::-1]):
         box = Square(
             fill_color=BLUE,
@@ -47,13 +56,17 @@ def sequenceBoxes(sequence):
             stroke_color=BLUE
         ).scale(0.5).shift((index-3) * LEFT + UP)
         text = Text(character, font_size=48).move_to(box.get_center())
-        boxGroup.add(box)
-        textGroup.add(text)
+        box_group.add(box)
+        text_group.add(text)
 
-    return boxGroup, textGroup
+    return box_group, text_group
 
 
 class CartesianProduct(Scene):
+    """
+    Main scene.
+    Animation explaining the cartesian product of the peptide mutator.
+    """
     def construct(self):
         sequence = "SKNKCNE"
 
@@ -65,42 +78,38 @@ class CartesianProduct(Scene):
         for index, character in enumerate(sequence):
             if index in position_list:
                 characters = [x[1] for x in mutations if x[0] == index]
-                sequential_mutations[index] = [x for x in characters[0]]
+                sequential_mutations[index] = list(characters[0])
             else:
                 sequential_mutations[index] = character
         print(sequential_mutations)
         seqs = itertools.product(*sequential_mutations)
 
-        sequenceList = VGroup()
+        sequence_list = VGroup()
         sequence_text = Text(sequence)
         sequence_text.to_corner(UP + RIGHT)
-        sequenceList.add(sequence_text)
-        sequenceList.to_corner(UP + RIGHT)
+        sequence_list.add(sequence_text)
+        sequence_list.to_corner(UP + RIGHT)
 
         for index, seq in enumerate(seqs):
             seq = "".join(seq)
 
-            seq_box_group, seq_text_group = sequenceBoxes(seq)
-            mut_main_groups, mut_box_groups, mut_text_groups = mutationBoxes(
+            seq_box_group, seq_text_group = sequence_boxes(seq)
+            mut_main_groups, mut_box_groups, mut_text_groups = mutation_boxes(
                     seq, seq_box_group, seq_text_group, mutations)
 
             self.add(seq_box_group)
             self.add(seq_text_group)
-            self.add(*[mut_box_group for mut_box_group in mut_box_groups])
-            self.add(*[mut_text_group for mut_text_group in mut_text_groups])
+            self.add(*mut_box_groups)
+            self.add(*mut_text_groups)
 
             sequence_text = Text(seq, font_size=36)
-            sequence_text.align_to(sequenceList[-1], UR).shift(0.5 * DOWN)
-            sequenceList.add(sequence_text)
+            sequence_text.align_to(sequence_list[-1], UR).shift(0.5 * DOWN)
+            sequence_list.add(sequence_text)
             self.play(Transform(seq_text_group, sequence_text), run_time=1)
             self.wait(0.5)
 
             self.play(mut_main_groups[0].animate.shift(UP))
 
             self.remove(*seq_box_group)
-            box_list = [box for box in 
-                [mut_box_group for mut_box_group in mut_box_groups]]
-            text_list = [text for text in 
-                [mut_text_group for mut_text_group in mut_text_groups]]
-            self.remove(*box_list)
-            self.remove(*text_list)
+            self.remove(*list(mut_box_groups))
+            self.remove(*list(mut_text_groups))
